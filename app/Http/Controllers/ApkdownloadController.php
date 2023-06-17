c31b050831d3eac68d2b8556a62b9c22d87256df
faruqhossen30 <faruqhossen30@gmail.com>
2023-06-18 05:05:05 +0600

    apk file name change

--- app/Http/Controllers/ApkdownloadController.php
+++ app/Http/Controllers/ApkdownloadController.php
@@ -8,7 +8,7 @@ use Response;
 class ApkdownloadController extends Controller
 {
     public function apkDownload(){
-        $filepath = public_path('uploads/apk/some.apk');
+        $filepath = public_path('uploads/apk/akashlive-v-1.0.0.apk');
         return Response::download($filepath);
     }
 }
