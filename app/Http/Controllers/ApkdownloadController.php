3a2fe9b4b9aa9e7ae46b01e86b112fda10938e01
faruqhossen30 <faruqhossen30@gmail.com>
2023-06-18 16:59:26 +0600

    apk download header added

--- app/Http/Controllers/ApkdownloadController.php
+++ app/Http/Controllers/ApkdownloadController.php
@@ -8,7 +8,12 @@ use Response;
 class ApkdownloadController extends Controller
 {
     public function apkDownload(){
+        $headers = [
+            'Content-Type' => 'application/vnd.android.package-archive',
+            'Content-Disposition' => 'attachment; filename=file.apk',
+        ];
+
         $filepath = public_path('uploads/apk/akashlive-v-1.0.0.apk');
-        return Response::download($filepath);
+        return Response::download($filepath,null,$headers);
     }
 }
