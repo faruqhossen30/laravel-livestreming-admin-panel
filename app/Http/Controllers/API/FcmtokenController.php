006b7bfbcdf4a595e5e803f3a86d2efb6662ce44
faruqhossen30 <faruqhossen30@gmail.com>
2023-10-04 12:37:11 +0600

    fcmtoken store api fuction update

--- app/Http/Controllers/API/FcmtokenController.php
+++ app/Http/Controllers/API/FcmtokenController.php
@@ -15,24 +15,10 @@ class FcmtokenController extends Controller
     {
         $user = $request->user();
         $request->validate(['fcmtoken' => 'required']);
-
-        if ($user->name_updated_at) {
-            $dt = Carbon::parse($user->name_updated_at);
-            $now = Carbon::now();
-            $diff = $dt->diffInDays($now);
-            if ($diff < 30) {
-                $after = 30 - intval($diff);
-                return response()->json([
-                    'success' => false,
-                    'code' => 422,
-                    'message' => "Please change your name after {$after} days",
-                ], 422);
-            }
-        }
         User::firstWhere('id', $user->id)->update(
             ['fcm_token' => $request->fcmtoken, 'fcm_time' => Carbon::now()]
         );
-        $user = User::firstWhere('id', $user->id);
+        // $user = User::firstWhere('id', $user->id);
 
         $db = app('firebase.firestore')->database();
         $firebaseUser = $db->collection('users')->document($user->id);
